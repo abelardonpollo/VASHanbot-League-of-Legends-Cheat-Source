102bc68b31cc29ae7e9bd57b3cd050b0d06c2dcd
reword3606 <xvdfgsd4435@outlook.com>
2025-03-19 19:22:56 +0400

    inline.h

---
+++ VASHanbot/utils/inline.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+
+#include <cstdint>
+
+namespace base { namespace hook {
+	typedef void* hook_t;
+	 bool install(uintptr_t* pointer_ptr, uintptr_t detour, hook_t* ph = nullptr);
+	 bool uninstall(hook_t* ph);
+}}
